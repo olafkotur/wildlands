f2a8055ce43f38b293373b1344b88db6ae918882
olafkotur <olafkotur@icloud.com>
2025-08-24 16:41:09 +0100

    Add inventory

--- scenes/interface/inventory/inventory.gd
+++ scenes/interface/inventory/inventory.gd
@@ -1,20 +1,37 @@
 extends Control
 
 @onready var inventory_panel = $PanelContainer
+@onready var close_button = $PanelContainer/CloseButton
+@onready var grid_container = $PanelContainer/MarginContainer/GridContainer
 
 func update_inventory():
 	var player_inventory = Inventory.get_inventory()
-	
-	for resource_name in player_inventory.keys():
-		var resource_amount = player_inventory[resource_name]
+
+	# Add items to the GridContainer
+	print(player_inventory)
+	for resource_enum in player_inventory.keys():
+		var resource_amount = player_inventory[resource_enum]
 		if resource_amount > 0:
+			var resource_name = Resources.ResourceMeta[resource_enum]["resource_name"]
 			var label = Label.new()
-			label.text = str(resource_name) + ": " + str(resource_amount)
-			inventory_panel.add_child(label)
+			label.text = "%s: %d" % [resource_name, resource_amount]
+			grid_container.add_child(label)
 			
 func toggle_visibility():
+	# Clear existing items in the GridContainer
+	if inventory_panel.visible:
+		for child in grid_container.get_children():
+			child.queue_free()
+
+	# Populate the GridContainer with updated inventory items
+	if not inventory_panel.visible:
+		update_inventory()
+
 	inventory_panel.visible = not inventory_panel.visible
-	print("New state:", inventory_panel.visible)
 
 func _ready():
+	close_button.connect("pressed", Callable(self, "_on_close_button_pressed"))
 	inventory_panel.visible = false
+
+func _on_close_button_pressed():
+	toggle_visibility()
